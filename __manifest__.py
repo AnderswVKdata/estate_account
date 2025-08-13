c8c5b4e7812d9aec8f2221412157698297e7dccc
Anders With <andersw@vkdata.dk>
2025-08-13 16:08:50 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Estate Account',
+    'version': '1.0',
+    'depends': ['estate', 'account'],
+    'author': 'Anders',
+    'category': 'Real Estate',
+    'description': 'Link module between Estate and Accounting.',
+    'installable': True,
+    'application': False,
+}
---
+++ models/estate_property.py
@@ -0,0 +1,35 @@
+from odoo import models
+from odoo.exceptions import UserError
+
+_inherit = "estate.property"
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def set_property_sold(self):
+        res = super().set_property_sold()
+        print("estate_account: property sold logic triggered")
+        for property in self:
+            partner = property.salesperson_id.partner_id.id
+            selling_price = property.selling_price
+
+
+            self.env['account.move'].create({
+                'partner_id': partner,
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    (0, 0, {
+                        'name': '6% Commission Fee',
+                        'quantity': 1.0,
+                        'price_unit': selling_price * 0.06,
+
+                    }),
+                    (0, 0, {
+                        'name': 'Administrative Fees',
+                        'quantity': 1.0,
+                        'price_unit': 100.0,
+                    }),
+                ]
+            })
+
+        return res
